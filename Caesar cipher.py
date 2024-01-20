5ca9d87a8b4d9e750fe8465dece91fcb3f41f558
Yuxuan Wang <156316303+13386336325@users.noreply.github.com>
2024-01-20 17:00:48 +0000

    Delete Caesar cipher.py

--- Caesar cipher.py
+++
@@ -1,9 +0,0 @@
-n,a=input().split(" ",1)
-for p in a:
-    if ord("a")<=ord(p)<=ord("z"):
-        print(chr(ord("a")+(ord(p)-ord("a")+int(n))%26),end='')
-    elif ord("A")<=ord(p)<=ord("Z"):
-        print(chr(ord("A")+(ord(p)-ord("A")+int(n))%26),end='')
-    else:
-        print(p,end='')
-        
